4c357782f5fbd0a8a92d6afc431dc80aeb14e6b7
chris <chriswilliams231@gmail.com>
2026-02-03 17:13:49 -0800

    New DB seeder

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
+use function Symfony\Component\Clock\now;
+
 class JobSeeder extends Seeder
 {
     /**
@@ -14,6 +16,23 @@ class JobSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //Loading some job listings from a file
+        $jobListings = include database_path('seeders/data/job_listings.php');
+
+        // Get user IDs from the User Model
+        $userIds = User::pluck('id')->toArray();
+
+        foreach ($jobListings as &$listing) {
+            // Assign user id to listing
+            $listing['user_id'] = $userIds[array_rand($userIds)];
+
+            // Add timestamps
+            $listing['created_at'] = now();
+            $listing['updated_at'] = now();
+        }
+
+        // Insert Job Listings
+        DB::table('job_listings')->insert($jobListings);
+         echo 'Jobs created successfully!';
     }
 }
