6924d0b02aa0a52aeeaa8aa3c0e526ca23a01034
chris <chriswilliams231@gmail.com>
2026-05-04 14:27:47 -0700

    updating login/register pages and login pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
-    <h1>Login Here</h1>
+    <div class="bg-white rounded-lg shadow-md w-full md:mx-width-xl mx-auto mt-12 p-8 py-12">
+        <h2 class="text-4xl text-center font-bold mb-4">Login Here</h2>
+    </div>
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,9 @@
 <x-layout>
-    <h1>Register is here</h1>
+    <div class="bg-white rounded-lg shadow-md w-full md:mx-width-xl mx-auto mt-12 p-8 py-12">
+        <h2 class="text-4xl text-center font-bold mb-4">Register Here</h2>
+        <form action="{{ route('register.store') }}" method="post">
+            @csrf
+            <x-inputs.text id="name" name="name" placeholder="Full Name" />
+        </form>
+    </div>
 </x-layout>
