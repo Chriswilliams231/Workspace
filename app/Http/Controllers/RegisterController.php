51a19d1d2b99d47e25c109b2fbd1dbc29ee4fd83
chris <chriswilliams231@gmail.com>
2026-05-04 21:17:01 -0700

    updating registration controller and view

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use Illuminate\HTTP\RedirectResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function register(): View {
         return view('auth.register');
+    }
+
+    // @desc Store user data in the database
+    // @route POST /register 
+    public function store(Request $request): RedirectResponse {
+        $validateData = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        // Hash password
+        $validateData['password'] = Hash::make($validateData['password']);
+
+        // Create user
+        $user = User::create($validateData);
+
+        return redirect()->route('login')->with('success', 'You are now registered. Please login.');
     }
 }
