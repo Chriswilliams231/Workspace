a9c31e5f36e52f3cd2a9530940ae733d9e8f3a64
chris <chriswilliams231@gmail.com>
2026-05-17 14:22:28 -0700

    adding the avatar

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
@@ -19,10 +20,26 @@ class ProfileController extends Controller
         $validatedData = $request->validate([
             'name' => 'required|string',
             'email' => 'required| string',
+            'avatar' => 'nullable|image|mimes:jpeg,jpg,png,gif|max:2048',
         ]);
+        
+        // Get single user input
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
 
+        // Handle avatar upload
+        if($request->hasFile('avatar')) {
+            // Delete the old image if it exists
+            if($user->avatar){
+                Storage::delete('public/' . $user->avatar);
+            }
+
+            // Storing new avatar
+            $avatarPath = $request->file('avatar')->store('avatars', 'public');
+            $user->avatar = $avatarPath;
+        }
         // Update user info
-        $user->update($validatedData);
+        $user->save();
 
 
         return redirect()->route('dashboard')->with('success', 'User info is updated');
--- resources/views/components/inputs/file.blade.php
+++ resources/views/components/inputs/file.blade.php
@@ -4,7 +4,7 @@
          <label class="block text-gray-700" for="{{ $id }}">{{ $label }}</label>
      @endif
      <input id="{{ $id }}" type="file" name="{{ $name }}"
-         class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror" />
+         class="w-full px-4 py-2 border rounded focus:outline-none cursor-pointer @error($name) border-red-500 @enderror" />
      @error($name)
          <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
      @enderror
