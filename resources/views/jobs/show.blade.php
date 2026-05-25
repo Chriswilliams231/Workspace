84e0ef0269b94ee6c31006338ec698848870b4aa
chris <chriswilliams231@gmail.com>
2026-05-25 16:43:55 -0700

    small update

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -85,7 +85,7 @@
                             Apply Now
                         </button>
 
-                        <div x-show="open" class="fixed inset-0 flex items-center justify-center bg-gray-900/50">
+                        <div x-cloak x-show="open" class="fixed inset-0 flex items-center justify-center bg-gray-900/50">
                             <div @click.away="open = false" class="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
                                 <h3 class="text-lg font-semibold mb-4 text-center">
                                     Apply for {{ $job->title }}
