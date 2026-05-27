4d872af67f37a79f2c65a4bf4761dd6b2d3dd5be
chris <chriswilliams231@gmail.com>
2026-05-27 14:21:05 -0700

    updating the search method

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\ApplicantController;
 // Get Methods 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 // Route::resource('jobs', JobController::class);
+Route::get('/jobs/search', [JobController::class, 'search'])->name('jobs.search');
 Route::resource('jobs', JobController::class)->middleware('auth')->only(['create','edit','update', 'destory']);
 Route::resource('jobs', JobController::class)->except(['create']);
 
